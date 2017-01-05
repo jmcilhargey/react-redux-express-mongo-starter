8523a9a6be5eeddc71456bb1a008fbb6c4c9810d
jmcilhargey <jmcilhargey@gmail.com>
2017-01-04 16:30:00 -0800

    Created container components, ininitialized store, need to add base reducers

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App Container</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/index";
+import thunk from "redux-thunk";
+import promise from "redux-promise";
+import createLogger from "redux-logger";
+
+const logger = createLogger();
+const persistedState = null;
+
+let store = createStore(rootReducer, applyMiddleware(thunk, promise));
+
+export default store;
