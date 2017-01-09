6c9ee8984cee01a535b4d556628c37168b14cafd
jmcilhargey <jmcilhargey@gmail.com>
2017-01-08 22:47:12 -0800

    Sample passport strategy for github

--- server/passport/index.js
+++ server/passport/index.js
@@ -1,3 +1,45 @@
-module.exports = function(app) {
-  
+const User = require("../models/User");
+const GitHubStrategy = require("passport-github").Strategy;
+const project = require("../../config/project.config");
+
+module.exports = function(app, passport) {
+
+  passport.use(new GitHubStrategy({
+      clientID: server.github.id,
+      clientSecret: server.github.secret,
+      callbackURL: `http://${ project.ip }:${ project.port }/auth/github/callback`
+    },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOne({ "github.id": profile.id }, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        let user = new User({
+          github: {
+            displayName: profile.displayName,
+            username: profile.username,
+            publicRepos: profile._json.public_repos,
+            accessToken: accessToken,
+          }
+        });
+        user.save((err, user) => {
+          if (err) {
+            return cb(err);
+          }
+          return cb(null, user);
+        });
+      } else {
+        return cb(null, user);
+      }
+    });
+  }));
+
+  passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+
+  passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
 }
