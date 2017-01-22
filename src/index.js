6a18c2d06220109512474fffc517c55afe9e3565
jmcilhargey <jmcilhargey@gmail.com>
2017-01-21 21:02:18 -0800

    Linked bundle in html, added localStorage option to preload state

--- src/index.js
+++ src/index.js
@@ -3,36 +3,46 @@ import "isomorphic-fetch";
 import React from "react";
 import ReactDOM from "react-dom";
 import { syncHistoryWithStore } from "react-router-redux";
-import { AppContainer } from "react-hot-loader";
 import { Router, browserHistory } from "react-router";
 import { Provider } from "react-redux";
-
-import store from "./store";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import promise from "redux-promise";
 import routes from "./routes";
-
+import { loadState, saveState } from "./utils/localStorage";
+import throttle from "lodash/throttle";
 require("./main.css");
-// console.log(window.__PRELOADED_STATE__);
 
-const Root = ({ store }) => (
+// Can use localStorage to persist state as well
+const persistedState = loadState();
+const preloadedState = window.__PRELOADED_STATE__;
+
+let store = createStore(rootReducer, preloadedState, applyMiddleware(thunk, promise));
+let history = syncHistoryWithStore(browserHistory, store);
+
+store.subscribe(throttle(() => {
+  saveState(store.getState());
+}, 1500));
+
+ReactDOM.render(
   <Provider store={ store }>
-    <Router history={ browserHistory } routes={ routes } onUpdate={ () => window.scrollTo(0, 0) } />
-  </Provider>
+    <Router history={ history } routes={ routes } onUpdate={ () => window.scrollTo(0, 0) } />
+  </Provider>,
+  document.getElementById("root")
 );
 
-const render = (Root) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Root store={ store } />
-    </AppContainer>,
-    document.getElementById("root")
-  );
-};
-
-render(Root);
-
-// Not working properly with React Router yet: https://github.com/gaearon/react-hot-loader/issues/249
+// Not working properly with React Router: https://github.com/gaearon/react-hot-loader/issues/249
+/*
+import { AppContainer } from "react-hot-loader";
 if (module.hot) {
   module.hot.accept(Root, () => {
-    render(Root);
+    ReactDOM.render(
+      <AppContainer>
+        <Root store={ store } />
+      </AppContainer>,
+      document.getElementById("root")
+    );
   });
 }
+*/
